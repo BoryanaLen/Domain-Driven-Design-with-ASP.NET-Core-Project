fb1ee8b3d0713e2946b03d1bfaafa20140790a21
Boryana Lengerova <b.arizanova@abv.bg>
2020-10-02 09:01:52 +0300

    Add Logout

---
+++ Core/Core.Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,41 @@
+﻿namespace Core.Web.Areas.Identity.Pages.Account
+{
+    using System.Threading.Tasks;
+    using Core.Infrastructure.Identity;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Logging;
+
+    [AllowAnonymous]
+    public class Logout : PageModel
+    {
+        private readonly SignInManager<User> signInManager;
+        private readonly ILogger<Logout> logger;
+
+        public Logout(SignInManager<User> signInManager, ILogger<Logout> logger)
+        {
+            this.signInManager = signInManager;
+            this.logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost(string returnUrl = null)
+        {
+            await this.signInManager.SignOutAsync();
+            this.logger.LogInformation("User logged out.");
+            if (returnUrl != null)
+            {
+                return this.LocalRedirect(returnUrl);
+            }
+            else
+            {
+                return this.RedirectToPage();
+            }
+        }
+    }
+}
